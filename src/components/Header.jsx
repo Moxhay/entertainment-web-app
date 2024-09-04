7017d54ddc3279b1976620455ba783a0e65a051d
Moxhay <alvarodmt14@gmail.com>
2024-09-04 18:12:28 +0200

    move nav buttons to component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,9 @@
-import NavButton from './NavButton.jsx'
-import MovieLogo from './icons/MovieLogo.jsx'
-import HomeIcon from './icons/HomeIcon.jsx'
-import MovieIcon from './icons/MovieIcon.jsx'
-import TvIcon from './icons/TvIcon.jsx'
-import Bookmark from './icons/Bookmark.jsx'
-import { useButtonColor } from '../helpers.jsx'
+import NavBar from './NavBar.jsx'
 
 const Header = () => {
-    const home = useButtonColor('/')
-    const movie = useButtonColor('/Movies')
-    const tv = useButtonColor('/TvShow')
-    const bookmark = useButtonColor('/Bookmark')
     return (
-        <header className="flex h-full justify-between bg-secondaryDarkBlue px-4 py-5 lg:px-10 xl:hidden">
-            <NavButton logo={<MovieLogo />} to={'/'} />
-            <div className="h-full items-center space-x-6 sm:space-x-8 xl:space-x-9">
-                <NavButton logo={<HomeIcon bg={home} />} to={'/'} />
-                <NavButton logo={<MovieIcon bg={movie} />} to={'/Movies'} />
-                <NavButton logo={<TvIcon bg={tv} />} to={'/TvShow'} />
-                <NavButton logo={<Bookmark bg={bookmark} />} to={'/Bookmark'} />
-            </div>
-            <div className="">
-                <img
-                    src="/img/img.png"
-                    alt="profile image"
-                    className="h-full w-full"
-                />
-            </div>
+        <header>
+            <NavBar />
         </header>
     )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import NavButton from './NavButton.jsx'
+import MovieLogo from './icons/MovieLogo.jsx'
+import HomeIcon from './icons/HomeIcon.jsx'
+import MovieIcon from './icons/MovieIcon.jsx'
+import TvIcon from './icons/TvIcon.jsx'
+import Bookmark from './icons/Bookmark.jsx'
+import { useButtonColor } from '../helpers.jsx'
+
+const NavBar = () => {
+    const home = useButtonColor('/')
+    const movie = useButtonColor('/Movies')
+    const tv = useButtonColor('/TvShow')
+    const bookmark = useButtonColor('/Bookmark')
+    return (
+        <div>
+            <div className="flex h-full justify-between bg-secondaryDarkBlue px-4 py-5 lg:px-10 xl:justify-start xl:rounded-full">
+                <NavButton logo={<MovieLogo />} to={'/'} />
+                <div className="flex h-full items-center space-x-6 sm:space-x-8 xl:space-x-9">
+                    <NavButton logo={<HomeIcon bg={home} />} to={'/'} />
+                    <NavButton logo={<MovieIcon bg={movie} />} to={'/Movies'} />
+                    <NavButton logo={<TvIcon bg={tv} />} to={'/TvShow'} />
+                    <NavButton
+                        logo={<Bookmark bg={bookmark} />}
+                        to={'/Bookmark'}
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/img/img.png"
+                        alt="profile image"
+                        className="h-full w-full"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default NavBar
