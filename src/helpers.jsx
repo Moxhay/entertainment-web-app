c93d250c7a827fde1a63e1e48a49c32923f1fc9e
Álvaro Dominguez Moxhay-Thorne <alvarodmt14@gmail.com>
2024-09-06 19:56:40 +0200

    update custom hook  useButtonColor add new feature to hook know who to work with diferent routes

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -2,5 +2,7 @@ import { useLocation } from 'react-router-dom'
 
 export const useButtonColor = (targetPath) => {
     const { pathname } = useLocation()
-    return pathname === targetPath ? 'white' : '#5A698F'
+    return pathname === targetPath || pathname.startsWith(`${targetPath}/`)
+        ? 'white'
+        : '#5A698F'
 }
