1f03f23c67d3ed3ff78cc3b73010852af8e9173e
Álvaro Dominguez Moxhay-Thorne <alvarodmt14@gmail.com>
2024-11-22 15:13:39 +0100

    add custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,22 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
-        colors: {
-            primaryRed: '#FC4747',
-            primaryDarkBlue: '#10141E',
-            secondaryDarkBlue: '#161D2F',
-            greyishBlue: '#5A698F',
-            primaryWhite: '#ffffff',
+        extend: {
+            colors: {
+                primaryRed: '#FC4747',
+                primaryDarkBlue: '#10141E',
+                primaryGrey: '#979797',
+                secondaryDarkBlue: '#161D2F',
+                greyishBlue: '#5A698F',
+                primaryWhite: '#ffffff',
+            },
+            fontFamily: {
+                Inter: ['Inter', 'sans-serif'],
+            },
+            screens: {
+                xs: '375px',
+            },
         },
+        plugins: [],
     },
-    plugins: [],
 }
