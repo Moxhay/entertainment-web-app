b39689877e905b0a18916d3ed181bf00a061fe91
moxhay <alvarodmt14@gmail.com>
2025-03-14 16:08:59 +0100

    delete unused helpers

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -1,65 +1,41 @@
-import Cookies from 'js-cookie'
-import ContentSection from '@components/ContentSection.jsx'
+import Cookies from 'js-cookie';
+import ContentSection from '@components/ContentSection.jsx';
 
 export const validateSlug = (slug) => {
-    const decodedSlug = decodeURIComponent(slug)
-    return (
-        typeof decodedSlug === 'string' &&
-        decodedSlug.length >= 1 &&
-        decodedSlug.length <= 100
-    )
-}
+    const decodedSlug = decodeURIComponent(slug);
+    return typeof decodedSlug === 'string' && decodedSlug.length >= 1 && decodedSlug.length <= 100;
+};
 
-export const fetchContentData = async ({
-    endPoint,
-    params,
-    setData,
-    dispatch,
-}) => {
-    try {
-        const response = await endPoint(params)
-        dispatch(setData(response.data))
-    } catch (error) {
-        console.error('Error:', error)
-    }
-}
-
-export const submitForm = ({
-    setLoading,
-    ApiEndPoint,
-    payload,
-    navigate,
-    setError,
-}) => {
-    setLoading(true)
+export const submitForm = ({ setLoading, ApiEndPoint, payload, navigate, setError }) => {
+    setLoading(true);
     ApiEndPoint(payload)
         .then((response) => {
-            setLoading(false)
+            setLoading(false);
 
             if (response) {
                 Cookies.set('jwt', response.data.jwt, {
                     path: '/',
                     expires: 7,
                     secure: true,
-                    sameSite: 'strict',
+                    sameSite: 'strict'
                 }),
                     Cookies.set('id', response.data.user.id, {
                         path: '/',
                         expires: 7,
                         secure: true,
-                        sameSite: 'strict',
-                    })
-                navigate('/')
+                        sameSite: 'strict'
+                    });
+                navigate('/');
             } else {
-                throw new Error(response.error)
+                throw new Error(response.error);
             }
         })
         .catch((error) => {
-            setLoading(false)
-            setError(true)
-            console.error(error)
-        })
-}
+            setLoading(false);
+            setError(true);
+            console.error(error);
+        });
+};
 
 export const transformResponse = (response) => {
     return response.data.map((item) => ({
@@ -71,64 +47,52 @@ export const transformResponse = (response) => {
         rated: item.rated,
         type: item.type,
         poster: item.cover_path?.url,
-        bookmarked: !!item.bookmarked,
-    }))
-}
+        bookmarked: !!item.bookmarkeds && item.bookmarkeds.length > 0
+    }));
+};
 
-export const bookmarkedStatus = ({
-    loadingMovies,
-    loadingSeries,
-    moviesError,
-    seriesError,
-    seriesData,
-    moviesData,
-    messageWhenEmpty,
-}) => {
-    if (loadingMovies || loadingSeries)
-        return (
-            <ContentSection>
-                <div>Loading...</div>
-            </ContentSection>
-        )
-    if (moviesError || seriesError)
-        return (
-            <ContentSection>
-                <div>Error loading movies or series</div>
-            </ContentSection>
-        )
+export const transformResponseForSingleMovie = (response) => {
+    return response.data.map((item) => ({
+        id: item.id,
+        documentId: item.documentId,
+        title: item.title,
+        trending: item.trending,
+        release_date: item.release_date,
+        rated: item.rated,
+        type: item.type,
+        poster: item.cover_path?.url,
+        movie_Url: item.movie_path,
+        description: item.description
+    }));
+};
+export const transformResponseForSingleSeries = (response) => {
+    return response.data.map((item) => ({
+        id: item.id,
+        documentId: item.documentId,
+        title: item.title,
+        trending: item.trending,
+        release_date: item.release_date,
+        rated: item.rated,
+        type: item.type,
+        poster: item.cover_path?.url,
+        description: item.description
+    }));
+};
+export const transformResponseForSingleSeriesSeasons = (response) => {
+    return response.data.map((item) => ({
+        id: item.id,
+        documentId: item.documentId,
+        SeasonTitle: item.SeasonTitle,
+        seasonsEpisodes: item.tv_episodes
+    }));
+};
 
-    if (moviesData.length < 1 && seriesData.length < 1)
+export const contentStatus = ({ isLoading, isFetching, error }) => {
+    if (isLoading || isFetching) return <ContentSection></ContentSection>;
+    if (error)
         return (
             <ContentSection>
-                <p>{messageWhenEmpty}</p>
+                <div className="h-fit w-fit text-white">Error loading movies or series</div>
             </ContentSection>
-        )
-}
-
-export const contentStatus = ({
-    data,
-    loading,
-    trendingLoading,
-    error,
-    trendingError,
-    messageWhenEmpty,
-}) => {
-    if (loading || trendingLoading)
-        return (
-            <ContentSection>
-                <div>Loading...</div>
-            </ContentSection>
-        )
-    if (error || trendingError)
-        return (
-            <ContentSection>
-                <div>error...</div>
-            </ContentSection>
-        )
-    if (data.length < 1)
-        return (
-            <ContentSection>
-                <div>{messageWhenEmpty}</div>
-            </ContentSection>
-        )
-}
+        );
+};
