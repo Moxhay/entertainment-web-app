8e41c4cf67e2596de6cfc4e745e860b5a82a2331
moxhay <alvarodmt14@gmail.com>
2025-03-14 16:07:53 +0100

    add new endpoint, and transformResponse

--- src/api/moviesApi.js
+++ src/api/moviesApi.js
@@ -1,71 +1,110 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import Cookies from 'js-cookie'
-import { baseUrl } from './api.js'
-import { transformResponse } from '../helpers.jsx'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import Cookies from 'js-cookie';
+import { baseUrl } from './api.js';
+import { transformResponse, transformResponseForSingleMovie } from '../helpers.jsx';
 
 export const moviesApi = createApi({
     reducerPath: 'moviesApi',
     baseQuery: fetchBaseQuery({
         baseUrl: baseUrl,
         prepareHeaders: (headers) => {
-            const token = Cookies.get('jwt')
+            const token = Cookies.get('jwt');
             if (token) {
-                headers.set('Authorization', `Bearer ${token}`)
+                headers.set('Authorization', `Bearer ${token}`);
             }
-            return headers
-        },
+            return headers;
+        }
     }),
     endpoints: (builder) => ({
         getMovies: builder.query({
             query: () => {
-                const userId = Cookies.get('id')
+                const userId = Cookies.get('id');
                 return {
                     url: 'movies',
                     params: {
-                        'populate[bookmarked][filters][users_permissions_user][id][$eq]':
-                            userId,
-                        populate: 'cover_path',
-                    },
-                }
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        populate: 'cover_path'
+                    }
+                };
             },
-            transformResponse: (response) => transformResponse(response),
+            transformResponse: (response) => transformResponse(response)
         }),
         getTrendingMovies: builder.query({
             query: () => {
-                const userId = Cookies.get('id')
+                const userId = Cookies.get('id');
                 return {
                     url: 'movies',
                     params: {
                         'filters[trending][$eq]': true,
-                        'populate[bookmarked][filters][users_permissions_user][id][$eq]':
-                            userId,
-                        populate: 'cover_path',
-                    },
-                }
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        populate: 'cover_path'
+                    }
+                };
             },
-            transformResponse: (response) => transformResponse(response),
+            transformResponse: (response) => transformResponse(response)
         }),
         getMovie: builder.query({
             query: (searchMovie) => {
-                const userId = Cookies.get('id')
+                const userId = Cookies.get('id');
                 return {
                     url: 'movies',
                     params: {
-                        'populate[bookmarked][filters][users_permissions_user][id][$eq]':
-                            userId,
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
                         populate: 'cover_path',
-                        'filters[title][$containsi]': searchMovie,
-                    },
-                }
+                        'filters[title][$containsi]': searchMovie
+                    }
+                };
             },
-
-            transformResponse: (response) => transformResponse(response),
+            transformResponse: (response) => transformResponse(response)
         }),
-    }),
-})
+        getBookmarkedMovies: builder.query({
+            query: () => {
+                const userId = Cookies.get('id');
+                return {
+                    url: `movies?filters[bookmarkeds][users_permissions_user][id][$eq]=${userId}&populate=cover_path&populate=bookmarked.movie.bookmarked`
+                };
+            },
+            transformResponse: (response) => transformResponse(response),
+            providesTags: ['getBookmarkedMovies']
+        }),
+        getSearchBookmarkedMovies: builder.query({
+            query: (searchMovie) => {
+                const userId = Cookies.get('id');
+                return {
+                    url: 'movies',
+                    params: {
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        'filters[bookmarkeds][$notNull]': true,
+                        populate: 'cover_path',
+                        'filters[title][$containsi]': searchMovie
+                    }
+                };
+            },
+            transformResponse: (response) => transformResponse(response)
+        }),
+        getSingleMovie: builder.query({
+            query: (searchMovie) => {
+                const userId = Cookies.get('id');
+                return {
+                    url: 'movies',
+                    params: {
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        'populate[cover_path]': true,
+                        'populate[movie_path]': true,
+                        'filters[title][$containsi]': searchMovie
+                    }
+                };
+            },
+            transformResponse: (response) => transformResponseForSingleMovie(response)
+        })
+    })
+});
 
 export const {
     useGetMoviesQuery,
     useGetTrendingMoviesQuery,
     useGetMovieQuery,
-} = moviesApi
+    useGetBookmarkedMoviesQuery,
+    useGetSearchBookmarkedMoviesQuery,
+    useGetSingleMovieQuery
+} = moviesApi;
--- src/api/seriesApi.js
+++ src/api/seriesApi.js
@@ -1,54 +1,129 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import Cookies from 'js-cookie'
-import { baseUrl } from './api.js'
-import { transformResponse } from '../helpers.jsx'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import Cookies from 'js-cookie';
+import { baseUrl } from './api.js';
+import { transformResponse, transformResponseForSingleSeries, transformResponseForSingleSeriesSeasons } from '../helpers.jsx';
 
 export const seriesApi = createApi({
     reducerPath: 'seriesApi',
     baseQuery: fetchBaseQuery({
         baseUrl: baseUrl,
         prepareHeaders: (headers) => {
-            const token = Cookies.get('jwt')
+            const token = Cookies.get('jwt');
 
             if (token) {
-                headers.set('Authorization', `Bearer ${token}`)
+                headers.set('Authorization', `Bearer ${token}`);
             }
 
-            return headers
-        },
+            return headers;
+        }
     }),
     endpoints: (builder) => ({
         getSeries: builder.query({
             query: () => {
-                const userId = Cookies.get('id')
+                const userId = Cookies.get('id');
                 return {
                     url: 'tv-series',
                     params: {
-                        'populate[bookmarked][filters][users_permissions_user][id][$eq]':
-                            userId,
-                        populate: 'cover_path',
-                    },
-                }
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        populate: 'cover_path'
+                    }
+                };
             },
             transformResponse: (response) => transformResponse(response),
-            providesTags: ['Series'],
+            providesTags: ['Series']
         }),
         getTrendingSeries: builder.query({
             query: () => {
-                const userId = Cookies.get('id')
+                const userId = Cookies.get('id');
                 return {
-                    url: 'movies',
+                    url: 'tv-series',
                     params: {
                         'filters[trending][$eq]': true,
-                        'populate[bookmarked][filters][users_permissions_user][id][$eq]':
-                            userId,
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        populate: 'cover_path'
+                    }
+                };
+            },
+            transformResponse: (response) => transformResponse(response)
+        }),
+        getSearchSeries: builder.query({
+            query: (searchSeries) => {
+                const userId = Cookies.get('id');
+                return {
+                    url: 'tv-series',
+                    params: {
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
                         populate: 'cover_path',
-                    },
-                }
+                        'filters[title][$containsi]': searchSeries
+                    }
+                };
+            },
+
+            transformResponse: (response) => transformResponse(response)
+        }),
+        getBookmarkedSeries: builder.query({
+            query: () => {
+                const userId = Cookies.get('id');
+                const queryParams = `Tv-series?filters[bookmarkeds][users_permissions_user][id][$eq]=${userId}&populate=cover_path&populate=bookmarked.movie.bookmarked`;
+                return {
+                    url: queryParams
+                };
             },
             transformResponse: (response) => transformResponse(response),
+            providesTags: ['getBookmarkedSeries']
         }),
-    }),
-})
+        getSearchBookmarkedSeries: builder.query({
+            query: (searchSeries) => {
+                const userId = Cookies.get('id');
+                return {
+                    url: 'tv-series',
+                    params: {
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        'filters[bookmarked][$notNull]': true,
+                        populate: 'cover_path',
+                        'filters[title][$containsi]': searchSeries
+                    }
+                };
+            },
+            transformResponse: (response) => transformResponse(response),
+            providesTags: ['getBookmarkedSeries']
+        }),
+        getSingleSeries: builder.query({
+            query: (documentId) => {
+                const userId = Cookies.get('id');
+                return {
+                    url: 'tv-series',
+                    params: {
+                        'filters[documentId][$eq]': documentId,
+                        'populate[bookmarkeds][filters][users_permissions_user][id][$eq]': userId,
+                        populate: 'cover_path'
+                    }
+                };
+            },
+            transformResponse: (response) => transformResponseForSingleSeries(response)
+        }),
+        getSingleSeriesSeason: builder.query({
+            query: (documentId) => {
+                return {
+                    url: 'tv-seasons',
+                    params: {
+                        'filters[tv_serie][documentId][$eq]': documentId,
+                        populate: 'tv_episodes.Tv_episode_path',
+                        sort: 'SeasonTitle:asc'
+                    }
+                };
+            },
+            transformResponse: (response) => transformResponseForSingleSeriesSeasons(response)
+        })
+    })
+});
 
-export const { useGetSeriesQuery, useGetTrendingSeriesQuery } = seriesApi
+export const {
+    useGetSeriesQuery,
+    useGetTrendingSeriesQuery,
+    useGetSearchSeriesQuery,
+    useGetBookmarkedSeriesQuery,
+    useGetSearchBookmarkedSeriesQuery,
+    useGetSingleSeriesQuery,
+    useGetSingleSeriesSeasonQuery
+} = seriesApi;
