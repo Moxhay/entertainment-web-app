1c734319ae52dfaa31bcfef1d7edd32c80259b86
moxhay <alvarodmt14@gmail.com>
2025-03-14 16:12:01 +0100

    delete delete unUsed middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { moviesApi } from '../api/moviesApi.js'
-import { seriesApi } from '../api/seriesApi.js'
-import { bookmarkedApi } from '../api/bookmarkedApi.js'
+import { configureStore } from '@reduxjs/toolkit';
+import { moviesApi } from '../api/moviesApi.js';
+import { seriesApi } from '../api/seriesApi.js';
 
 export const store = configureStore({
     reducer: {
         [moviesApi.reducerPath]: moviesApi.reducer,
-        [seriesApi.reducerPath]: seriesApi.reducer,
-        [bookmarkedApi.reducerPath]: bookmarkedApi.reducer,
+        [seriesApi.reducerPath]: seriesApi.reducer
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(
-            moviesApi.middleware,
-            seriesApi.middleware,
-            bookmarkedApi.middleware
-        ),
-})
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(moviesApi.middleware, seriesApi.middleware)
+});
