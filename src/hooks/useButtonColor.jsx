f74972f36889b023441bbf60453e8aeaeb79d6dc
moxhay <alvarodmt14@gmail.com>
2025-03-14 16:04:10 +0100

    fix when button need to change color

--- src/hooks/useButtonColor.jsx
+++ src/hooks/useButtonColor.jsx
@@ -1,8 +1,14 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 
 export const useButtonColor = (targetPath) => {
-    const { pathname } = useLocation()
-    return pathname === targetPath || pathname.startsWith(`${targetPath}/`)
+    const { pathname } = useLocation();
+    const targetPathLowerCase = targetPath.toLowerCase();
+    const pathLowerCase = pathname.toLowerCase();
+
+    return pathLowerCase === targetPathLowerCase ||
+        pathLowerCase.includes(`${targetPathLowerCase}/`) ||
+        pathLowerCase.startsWith(`${targetPathLowerCase}search`) ||
+        pathLowerCase.includes(`${targetPathLowerCase}/search`)
         ? 'white'
-        : '#5A698F'
-}
+        : '#5A698F';
+};
