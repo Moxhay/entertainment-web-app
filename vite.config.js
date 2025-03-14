cf77a8223278d7cc298335fc14c164015b84f328
moxhay <alvarodmt14@gmail.com>
2025-03-14 15:58:54 +0100

    Update to tailwind 4 and install material UI

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            // eslint-disable-next-line no-undef
+            '@': path.resolve(__dirname, 'src'),
             '@components': path.resolve(__dirname, 'src/components'),
-        },
-    },
-})
+            '@hooks': path.resolve(__dirname, 'src/hooks')
+        }
+    }
+});
