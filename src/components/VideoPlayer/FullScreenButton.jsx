2957d494c0b0ba34fa9cf680615a8bcbbc7ba521
moxhay <alvarodmt14@gmail.com>
2025-03-15 17:05:27 +0100

    delete unused components and hooks

--- src/components/VideoPlayer/FullScreenButton.jsx
+++
@@ -1,7 +0,0 @@
-import { RxEnterFullScreen, RxExitFullScreen } from 'react-icons/rx';
-
-export const FullScreenButton = ({ isFullscreen, toggleFullScreen }) => (
-    <button onClick={toggleFullScreen} className="text-primaryRed z-40 self-end p-3">
-        {isFullscreen ? <RxExitFullScreen className="size-6 cursor-pointer" /> : <RxEnterFullScreen className="size-6 cursor-pointer" />}
-    </button>
-);
