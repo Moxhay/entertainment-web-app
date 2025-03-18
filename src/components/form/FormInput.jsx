576ad1233f4758abbe79fbcf84d5d28a03319b55
moxhay <alvarodmt14@gmail.com>
2025-03-18 18:48:04 +0100

    fix

--- src/components/form/FormInput.jsx
+++ src/components/form/FormInput.jsx
@@ -1,21 +1,12 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const InputLabel = ({
-    type,
-    name,
-    value,
-    onChange,
-    onBlur,
-    errors,
-    touched,
-    placeholder,
-}) => {
+const InputLabel = ({ type, name, value, onChange, onBlur, errors, touched, placeholder }) => {
     return (
         <label
             className={
                 errors && touched
-                    ? 'flex w-full flex-row border-b border-b-primaryRed bg-inherit py-3'
-                    : 'flex w-full flex-row border-b border-b-greyishBlue bg-inherit py-3 text-primaryWhite focus-within:border-b-primaryWhite'
+                    ? 'border-b-primaryRed flex w-full flex-row border-b bg-inherit py-3'
+                    : 'border-b-greyishBlue text-primaryWhite focus-within:border-b-primaryWhite flex w-full flex-row border-b bg-inherit py-3'
             }
         >
             <input
@@ -25,19 +16,19 @@ const InputLabel = ({
                 onChange={onChange}
                 onBlur={onBlur}
                 value={value}
-                className="bg-inherit bg-origin-content pl-5 font-Inter text-sm text-primaryWhite placeholder-primaryGrey caret-red-500 outline-hidden autofill:bg-none focus:text-primaryWhite focus:ring-0"
+                className="font-Inter text-primaryWhite placeholder-primaryGrey focus:text-primaryWhite bg-inherit bg-origin-content pl-5 text-sm caret-red-500 outline-hidden autofill:bg-none focus:ring-0"
                 placeholder={placeholder}
             />
             {errors && touched && (
-                <div className="bg-inherit font-Inter text-xs text-primaryRed hover:text-primaryRed focus:text-primaryRed active:text-primaryRed">
+                <div className="font-Inter text-primaryRed hover:text-primaryRed focus:text-primaryRed active:text-primaryRed w-full self-end bg-inherit text-end text-xs">
                     {errors}
                 </div>
             )}
         </label>
-    )
-}
+    );
+};
 
-export default InputLabel
+export default InputLabel;
 InputLabel.propTypes = {
     type: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
@@ -46,5 +37,5 @@ InputLabel.propTypes = {
     onBlur: PropTypes.func.isRequired,
     errors: PropTypes.string,
     touched: PropTypes.bool,
-    placeholder: PropTypes.string,
-}
+    placeholder: PropTypes.string
+};
