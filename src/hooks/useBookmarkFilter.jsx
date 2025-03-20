3101ae6628d47877b4f0e7101683b9d2b92926ba
moxhay <alvarodmt14@gmail.com>
2025-03-20 15:23:03 +0100

    add useCallBack

--- src/hooks/useBookmarkFilter.jsx
+++ src/hooks/useBookmarkFilter.jsx
@@ -2,51 +2,37 @@ import useBookmark from './useBookmark.jsx';
 import { useDispatch } from 'react-redux';
 import { moviesApi, useGetBookmarkedMoviesQuery } from '@/api/moviesApi.js';
 import { seriesApi, useGetBookmarkedSeriesQuery } from '@/api/seriesApi.js';
+import { useCallback } from 'react';
 
 export function useBookmarkFilter() {
-    const { data: movies, isLoading: isLoadingMovies, error: errorMovies, refetch: refetchMovies } = useGetBookmarkedMoviesQuery();
-    const { data: series, isLoading: isLoadingSeries, error: errorSeries, refetch: refetchSeries } = useGetBookmarkedSeriesQuery();
+    const { data: movies, isLoading: isLoadingMovies, error: errorMovies } = useGetBookmarkedMoviesQuery();
+    const { data: series, isLoading: isLoadingSeries, error: errorSeries } = useGetBookmarkedSeriesQuery();
     const { loadingBookmarked, handleBookmarked } = useBookmark();
     const dispatch = useDispatch();
 
-    const handleBookmark = async ({ id, currentBookmarkedState, type }) => {
-        const endpointName = type === 'Movie' ? 'getBookmarkedMovies' : 'getBookmarkedSeries';
-        const data = type === 'Movie' ? movies : series;
-        const prevState = data;
+    const handleBookmark = useCallback(
+        async ({ id, currentBookmarkedState, type }) => {
+            const endpointName = type === 'Movie' ? 'getBookmarkedMovies' : 'getBookmarkedSeries';
+            const data = type === 'Movie' ? movies : series;
+            const prevState = data;
+            const updatedData = data.filter((content) => content.id !== id);
 
-        const updatedData = data.filter((content) => content.id !== id);
-        console.log(type);
-        if (type === 'Movie') {
-            console.log(updatedData);
-            await dispatch(
-                moviesApi.util.updateQueryData(`${endpointName}`, undefined, () => {
-                    return updatedData;
-                })
-            );
-        } else {
-            await dispatch(
-                seriesApi.util.updateQueryData(`${endpointName}`, undefined, () => {
-                    return updatedData;
-                })
-            );
-        }
-
-        await handleBookmarked({ id, currentBookmarkedState, type }).catch(() => {
-            if (type === 'movies') {
-                dispatch(
-                    moviesApi.util.updateQueryData(`${endpointName}`, undefined, () => {
-                        return prevState;
-                    })
-                );
+            if (type === 'Movie') {
+                await dispatch(moviesApi.util.updateQueryData(endpointName, undefined, () => updatedData));
             } else {
-                dispatch(
-                    seriesApi.util.updateQueryData(`${endpointName}`, undefined, () => {
-                        return prevState;
-                    })
-                );
+                await dispatch(seriesApi.util.updateQueryData(endpointName, undefined, () => updatedData));
             }
-        });
-    };
+
+            await handleBookmarked({ id, currentBookmarkedState, type }).catch(() => {
+                if (type === 'Movie') {
+                    dispatch(moviesApi.util.updateQueryData(endpointName, undefined, () => prevState));
+                } else {
+                    dispatch(seriesApi.util.updateQueryData(endpointName, undefined, () => prevState));
+                }
+            });
+        },
+        [dispatch, handleBookmarked, movies, series]
+    );
 
     return {
         isLoading: isLoadingMovies || isLoadingSeries,
