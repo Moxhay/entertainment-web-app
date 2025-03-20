3acc8288d49d4f8f08e1cdb77e7ef785d9b1e5a0
moxhay <alvarodmt14@gmail.com>
2025-03-20 16:33:19 +0100

    add Group Hover

--- src/components/VideoPlayer/VideoPlayerControlsContainer.jsx
+++ src/components/VideoPlayer/VideoPlayerControlsContainer.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export const VideoPlayerControlsContainer = ({ children }) => {
     return (
-        <div className="absolute inset-0 flex flex-col items-center self-end overflow-hidden px-2 opacity-100 sm:inset-2 sm:px-0 lg:inset-4">
+        <div className="absolute inset-0 flex flex-col items-center self-end overflow-hidden px-2 opacity-0 group-hover:opacity-100 sm:inset-2 sm:px-0 lg:inset-4">
             {children}
         </div>
     );
