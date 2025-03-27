affbd9981ccfca263ac104b17ca98340e7ea2aa6
moxhay <alvarodmt14@gmail.com>
2025-03-27 15:19:30 +0100

    adjust transition speed

--- src/components/Transitions/TransitionEnterExitVariants/TransitionEnterExitWithAnimationState.jsx
+++ src/components/Transitions/TransitionEnterExitVariants/TransitionEnterExitWithAnimationState.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 export function TransitionEnterExitWithAnimationState({ children, setAnimationState, animationState }) {
     return (
         <motion.div
-            initial={{ opacity: 0, x: '-100%' }}
+            initial={{ opacity: 0, x: '-25%' }}
             animate={{
                 opacity: 1,
                 x: 0
             }}
-            exit={{ opacity: 0, x: '200%' }}
-            transition={{ duration: 4, ease: 'easeInOut' }}
+            exit={{ opacity: 0.5, x: '100%' }}
+            transition={{ duration: 2, ease: 'easeInOut' }}
             onAnimationComplete={() => setAnimationState(!animationState)}
         >
             {children}
